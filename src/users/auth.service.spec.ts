194024b91d62038f9385832dc2a96d8c9d809cc8
SomkamolV <somkamolv@hotmail.com>
2025-03-13 23:12:50 +0700

    section_11 => add test case throws an error if user signs up with email that is in use.

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,13 +2,14 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService Unit Test',() => {
-    let service: AuthService;
-
+    let authService: AuthService;
+    let fakeUserService: Partial<UsersService>;
     beforeEach(async() => {
         //Create a fake copy of the users service
-        const fakeUserService: Partial<UsersService> = {
+        fakeUserService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) =>  
                 Promise.resolve({id:1 , email, password} as User                    
@@ -25,21 +26,30 @@ describe('AuthService Unit Test',() => {
             ]
         }).compile();
 
-        service = module.get(AuthService);
+        authService = module.get(AuthService);
 
     })
 
     it('Can create an instance of auth service', async() => {
 
-        expect(service).toBeDefined();
+        expect(authService).toBeDefined();
     });
 
     it('create a new user with a salted and hased password', async() => {
 
-        const user = await service.signUp('asdf@asdf.com','asdf');
+        const user = await authService.signUp('asdf@asdf.com','asdf');
         expect(user.password).not.toEqual('asdf');
         const [salt,hash] = user.password.split('.');
         expect(salt).toBeDefined();
         expect(hash).toBeDefined();
     });
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        fakeUserService.find = () =>  
+          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await expect(authService.signUp('asdf@asdf.com', 'asdf')).rejects.toThrow(
+          BadRequestException,
+        );
+      });
 });
+
