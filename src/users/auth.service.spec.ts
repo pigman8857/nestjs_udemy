68cfcd59dd4b5f326c53b4b3448b4736496ad365
SomkamolV <somkamolv@hotmail.com>
2025-03-13 23:41:09 +0700

    section_11 => add test cases that sign in failed for unused mail and password invalid case.

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService Unit Test',() => {
     let authService: AuthService;
@@ -47,9 +47,27 @@ describe('AuthService Unit Test',() => {
     it('throws an error if user signs up with email that is in use', async () => {
         fakeUserService.find = () =>  
           Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        
         await expect(authService.signUp('asdf@asdf.com', 'asdf')).rejects.toThrow(
           BadRequestException,
         );
+    });
+
+    it('throws if signin is called with an unused email', async () => {
+        await expect(
+            authService.signIn('asdflkj@asdlfkj.com', 'passdflkj'),
+        ).rejects.toThrow(NotFoundException);
+    });
+
+    it('throws if an invalid password is provided', async () => {
+        fakeUserService.find = () =>
+          Promise.resolve([
+            { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
+          ]);
+        await expect(
+            authService.signIn('laskdjf@alskdfj.com', 'passowrd'),
+        ).rejects.toThrow(BadRequestException);
       });
+    
 });
 
