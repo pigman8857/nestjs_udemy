b5800cd64466d191a716d3dc375c6eae225e408a
SomkamolV <somkamolv@hotmail.com>
2025-03-17 22:26:16 +0700

    section_14 => test case Sign Up as a new user then get the currently logged in user.

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -18,14 +18,32 @@ describe('Authentication System (e2e)', () => {
   it('Handles a signup request', () => {
     const email = 'asdlkjq4321@akl.com';
 
-    return request(app.getHttpServer(),)
+    return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({email, password : 'alskdfjl'})
+      .send({ email, password: 'alskdfjl' })
       .expect(201)
       .then((res) => {
-        const {id , email} = res.body;
+        const { id, email } = res.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
-      })
+      });
+  });
+
+  it('Sign Up as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'alskdfjl' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
