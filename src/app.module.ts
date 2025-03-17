ea4140bf13b9860d61ccf59fc6695074a84f7052
SomkamolV <somkamolv@hotmail.com>
2025-03-17 18:18:24 +0700

    secrion_13 => configure middleware at app.module and set up validationPipe at app.module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+const cookieSession = require('cookie-session');
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
@@ -16,6 +18,25 @@ import { Report } from './reports/report.entity';
   UsersModule, 
   ReportsModule, ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        //Trip out the unwanted property in request body
+        whitelist: true
+      })
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer : MiddlewareConsumer){
+    console.log('AppModule.configure()');
+    consumer.apply(
+      cookieSession({
+        keys: ['asdfasfd']
+      })
+    ).forRoutes('*');
+
+  }
+}
