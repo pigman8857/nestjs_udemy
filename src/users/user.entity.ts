330c0ad6675902b46901f331d738c13b78763bd2
SomkamolV <somkamolv@hotmail.com>
2025-03-19 23:22:09 +0700

    section_15 => add relation one-To-many and many-to-one for Report and User entities.

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,30 +1,43 @@
-
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate} from "typeorm";
+import { Report } from 'src/reports/report.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  OneToMany
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @AfterInsert()
-    logInsert(){
-        console.log('Inserted user with id',this.id);
-    }
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
-    @AfterUpdate()
-    logUpdate(){
-        console.log('Updated user with id',this.id);
-    }
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
 
-    @AfterRemove()
-    logRemove(){
-        console.log('remove user with id',this.id);
-    }
-    
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('remove user with id', this.id);
+  }
+
+
+
 }
