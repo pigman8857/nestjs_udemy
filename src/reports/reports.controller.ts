5dab3e7db9c303fd88c8bc69fb03b24c589f6a53
SomkamolV <somkamolv@hotmail.com>
2025-03-20 00:29:26 +0700

    section_15 => Finished. Add report DTO to rersponds from create report API. When create Report, also add association with User entity as well in ReportsService.create(). Do not expose password when create Report but only returns with userId.

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Post,Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService  } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
-
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDTO } from './dtos/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -10,8 +13,9 @@ export class ReportsController {
 
     @Post()
     @UseGuards(AuthGuard)
-    createReport(@Body()body: CreateReportDto){
+    @Serialize(ReportDTO)
+    createReport(@Body()body: CreateReportDto,@CurrentUser() user: User ){
         console.log('createReport()',body);
-        return this.reportsService.create(body);
+        return this.reportsService.create(body, user);
     }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,13 +3,15 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report)private repo: Repository<Report>){}
 
-    create(createReportDTO: CreateReportDto){
+    create(createReportDTO: CreateReportDto, user: User){
         const report = this.repo.create(createReportDTO);
+        report.user = user;
         return this.repo.save(report);
     }
 }
