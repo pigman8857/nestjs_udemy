86caeacccf5f9808546f5bd83ef0fc385b1c9574
SomkamolV <somkamolv@hotmail.com>
2025-03-19 17:14:50 +0700

    section_15 => implemnet creation Report service method and Route API

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post,Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService  } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportsService: ReportsService){}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body()body: CreateReportDto){
+        console.log('createReport()',body);
+        return this.reportsService.create(body);
+    }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    constructor(@InjectRepository(Report)private repo: Repository<Report>){}
+
+    create(createReportDTO: CreateReportDto){
+        const report = this.repo.create(createReportDTO);
+        return this.repo.save(report);
+    }
+}
