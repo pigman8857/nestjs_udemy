8b67748cc22f3d35158c5fe7c792fa2ca7421666
SomkamolV <somkamolv@hotmail.com>
2025-03-23 00:22:22 +0700

    secrtion_16 => new dto and added dtos index.

---
+++ src/reports/dtos/index.ts
@@ -0,0 +1,12 @@
+import { ApproveRequestDTO } from "./approve-report.dto";
+import { CreateReportDto } from "./create-report.dto";
+import { GetEstimateDto } from "./get-estimate.dto";
+import { ReportDTO } from "./report.dto";
+
+
+export {
+    ApproveRequestDTO,
+    CreateReportDto,
+    GetEstimateDto,
+    ReportDTO
+}
