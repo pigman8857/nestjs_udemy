ce60f340a72d080e1b3b2869e5330d99c8b58292
SomkamolV <somkamolv@hotmail.com>
2025-03-26 14:53:55 +0700

    section_18 => typeorm.config.ts -> TypeOrmModuleOptions have different property depends on node_env. isntall pg driver

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,12 +10,52 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
 
     createTypeOrmOptions(connectionName?: string): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
 
-        return {
-            type: this.configService.get<"postgres" | "sqlite">('DB_TYPE'),
-            synchronize: this.configService.get<boolean>('SYNCHRONIZE'),
-            database: this.configService.get<string>('DB_NAME'),
-            migrationsRun: this.configService.get<boolean>('MIGRATIONS_RUN'),
-            autoLoadEntities: true
+
+        const type = this.configService.get<"postgres" | "sqlite">('DB_TYPE');
+        const synchronize = this.configService.get<boolean>('SYNCHRONIZE');
+        const database = this.configService.get<string>('DB_NAME');
+        const url = this.configService.get<string | null>('DB_URL');
+        const migrationsRun = this.configService.get<boolean>('MIGRATIONS_RUN');
+
+        //heroku
+        const ssl = { rejectUnauthorized: false}
+
+        let option: TypeOrmModuleOptions = {};
+
+        const env = process.env.NODE_URL;
+        switch(env){
+            case 'test':
+                option = {
+                    type,
+                    synchronize,
+                    database,
+                    migrationsRun,
+                    autoLoadEntities: true
+                };
+                break;
+            case 'development':
+                option = {
+                    type,
+                    synchronize,
+                    database,
+                    migrationsRun,
+                    autoLoadEntities: true
+                };
+                break;
+            case 'production':
+                option = {
+                    type : 'postgres',
+                    synchronize,
+                    ssl,
+                    url,
+                    migrationsRun,
+                    autoLoadEntities: true
+                };
+                break;
+            default:
+                throw new Error('unknown environment');
         }
+
+        return option; 
     }
 }
